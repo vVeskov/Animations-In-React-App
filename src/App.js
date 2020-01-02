2dd8b844df63d6e66fb433e3cc687a848443d830
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2020-01-02 16:37:52 +0200

    Animation Timing added !

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Transition from 'react-transition-group/Transition'
+import Transition from 'react-transition-group/Transition';
 import "./App.css";
 import Modal from "./components/Modal/Modal";
 import Backdrop from "./components/Backdrop/Backdrop";
@@ -45,11 +45,9 @@ class App extends Component {
             </div>
           )}
         </Transition>
-        <Transition in={this.state.modalIsOpen} timeout={300} mountOnEnter unmountOnExit>
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
-          )}
-        </Transition>
+
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+
         {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
