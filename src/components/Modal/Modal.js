2351e9f19ec0cfdd97d0d4a92a798a050fc6e082
Valentin Chobadzhakov <valentin.chobadzhakov@gmail.com>
2020-01-02 17:01:34 +0200

    Customizing CSS Classes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Transition from 'react-transition-group/Transition';
+import CSSTransition from 'react-transition-group/CSSTransition';
 
 import './Modal.css';
 
@@ -10,21 +10,24 @@ const animationTiming = {
 
 const modal = (props) => {
     return (
-        <Transition in={props.show} timeout={animationTiming} mountOnEnter unmountOnExit>
-            {state => {
-                const cssClasses = [
-                    'Modal',
-                    state === 'entering' ? 'ModalOpen'
-                        : state === 'exiting' ? 'ModalClosed' : null
-                ]
-                return (
-                    <div className={cssClasses.join(' ')}>
-                        <h1>A Modal</h1>
-                        <button className="Button" onClick={props.closed}>Dismiss</button>
-                    </div>
-                )
-            }}
-        </Transition>
+        <CSSTransition
+            in={props.show}
+            timeout={animationTiming}
+            mountOnEnter
+            unmountOnExit
+            classNames={{
+                enter: '',
+                enterActive: 'ModalOpen',
+                exit: '',
+                exitActive: 'ModalClosed',
+            }}>
+
+            <div className="Modal">
+                <h1>A Modal</h1>
+                <button className="Button" onClick={props.closed}>Dismiss</button>
+            </div>
+
+        </CSSTransition >
     )
 };
 
